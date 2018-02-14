c8e40f8023f1dd181d04fbea64e7f2312037c2d3
Simon Guillorit <simon.guillorit@gmail.com>
2018-02-14 19:46:20 +0100

    Fix params typo

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -1,12 +1,18 @@
 module Api::V1
   class IdeasController < ApplicationController
     def index
-      @ideas = Idea.all
+      @ideas = Idea.order("created_at DESC")
       render json: @ideas
     end
 
     def create
       @idea = Idea.create(idea_params)
+      render json: @idea
+    end
+
+    def update
+      @idea = Idea.find(params[:id])
+      @idea.update_attributes(idea_params)
       render json: @idea
     end
 
